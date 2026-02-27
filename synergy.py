4207343cae619d991a7de4504d406f520b57de5e
dekr0o0 <60785612+dekr0o0@users.noreply.github.com>
2026-02-27 10:13:01 +0300

    Добавить начальный скрипт с примерами вывода и арифметики на Python

---
+++ synergy.py
@@ -0,0 +1,51 @@
+print(5)
+print("abcd")
+print("abcd", "asdqwe")
+print("asdzxc", 123,sep="-")
+x=8
+x=9
+print(x)
+a_b=10
+print(a_b)
+# это моё первое знакомство с пайтоном
+#арифметика
+a = 5
+a = 5 + 6
+b = 6 - 7
+c = 4 * 5
+d = 7 // 5
+e = 7 % 5
+f = 2 ** 3
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+a = 5
+a += 3
+print(a)
+a = int(input("Введите число: "))
+print(a + 6)
+a = int(input("Введите число: "))
+print(a)
+b, d = map(int, input("Введите два числа через пробел: ").split())
+a, b, c = input().split()
+a = int(a)
+print(a * 2)
+a = int(input())
+b = int(input())
+c = a + b
+print(c)
+a, b = map(int, input().split())
+c = a + b
+print(c)
+a = 10
+b = 3
+print(a / b)
+# логические операторы и условия
+a = 5
+b = 6
+
+print(a < b)
+print(a > b)
